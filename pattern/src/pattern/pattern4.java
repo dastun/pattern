77f90a6393956ca0e5656e9727c2efed0d2b2cc4
dastun <53134652+dastun@users.noreply.github.com>
2022-08-23 23:17:38 +0530

    Add files via upload

---
+++ pattern/src/pattern/pattern4.java
@@ -0,0 +1,24 @@
+package pattern;
+
+public class pattern4 {
+	 public static void main(String[] args)
+	    {
+	        // number n for which we have to print the
+	        // multiplication table.
+	        int N = 3;
+	  
+	        // looping from 1 to 10 to print the multiplication
+	        // table of the number.
+	        // using for loop
+	        for (int i = 1; i <= 15; i++) {
+	            // printing the N*i,ie ith multiple of N.
+	            System.out.println(N + " * " + i + " = "
+	                               + N * i);
+	        }
+	    }
+	
+	
+	
+	
+}
+
